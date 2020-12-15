20879f3c150a5416eb6fc124a7252ecddb5d1738
Mayurkumar Bhayani <maybhaya@publicisgroupe.net>
2020-12-15 14:01:29 -0400

    Second Commit

---
+++ src/main/java/com/example/springcosmos/SpringcosmosApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcosmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcosmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcosmosApplication.class, args);
+	}
+
+}
